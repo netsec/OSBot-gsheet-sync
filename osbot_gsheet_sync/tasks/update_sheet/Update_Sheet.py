0f4130ebb546323c45f7de869150eb463f352c8a
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-15 22:49:59 +0100

    refactored trigger task method

--- osbot_gsheet_sync/tasks/update_sheet/Update_Sheet.py
+++ osbot_gsheet_sync/tasks/update_sheet/Update_Sheet.py
@@ -5,7 +5,8 @@ from pbx_gs_python_utils.utils.Misc import Misc
 class Update_Sheet:
 
     def log_message(self):
-        message = Misc.random_string_and_numbers(prefix='this is a message from code build - ')
+
+        message = Misc.random_string_and_numbers(prefix='....this is a message from code build - ')
         channel = 'DDKUZTK6X'
         team_id = 'T7F3AUXGV'
         slack_message(message, [], channel, team_id)
--- run/test_Trigger_Builds.py
+++ run/test_Trigger_Builds.py
@@ -12,13 +12,18 @@ class test_Trigger_Builds(TestCase):
         self.account_id = IAM().account_id()
         self.api = Create_Code_Build(account_id=self.account_id, project_name=self.project_name)
 
-    def test_trigger_Update_Sheet(self):
+    def trigger_task(self,task_name):
         kvargs = {
-                    'projectName'       : self.project_name,
-                    'buildspecOverride' : 'osbot_gsheet_sync/update_sheet/buildspec.yml'
-                  }
-        result = self.api.code_build.codebuild.start_build(**kvargs)#.get('build').get('arn')
+            'projectName'      : self.project_name,
+            'buildspecOverride': 'osbot_gsheet_sync/tasks/{0}/buildspec.yml'.format(task_name),
+            'artifactsOverride': {'name': 'AAAAAAAAA'}
+            #'sourceVersion'    : task_name
+        }
+        return self.api.code_build.codebuild.start_build(**kvargs).get('build')
 
+    def test_trigger_Update_Sheet(self):
+
+        result = self.trigger_task('update_sheet')
         Dev.pprint(result)
 
 
